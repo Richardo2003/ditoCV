0211ec905cab81630473b0c781723b11d80e8391
Richardo2003 <s22110407@student.unklab.ac.id>
2023-11-18 10:45:43 +0700

    cv

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Homepage from "./components/homepage"
+import "./assets/css/style.css"
+
+const App = () => {
+ 
+  return (
+    <>
+    <Homepage/>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/homepage.jsx
@@ -0,0 +1,181 @@
+const Homepage = () => {
+    return (
+        <>
+<section className="main-section">
+  <div className="left-part">
+    <div className="photo-container">
+      <img src="img/face.png" alt />
+    </div>
+    <div className="contact-container">
+      <h2 className="title">Contact Me</h2>
+      <div className="contact-list">
+        <div className="icon-container">
+          <i className="bi bi-geo-alt-fill" />
+        </div>
+        <div className="contact-text">
+          <p>F43C+89R Danowudu, Kota Bitung, Sulawesi Utara</p>
+        </div>
+      </div>
+      <div className="contact-list">
+        <div className="icon-container">
+          <i className="bi bi-facebook" />
+        </div>
+        <div className="contact-text">
+          <p>facebook.com/dito</p>
+        </div>
+      </div>
+      <div className="contact-list">
+        <div className="icon-container">
+          <i className="bi bi-instagram" />
+        </div>
+        <div className="contact-text">
+          <p>instagram.com/ditoemor</p>
+        </div>
+      </div>
+      <div className="contact-list">
+        <div className="icon-container">
+          <i className="bi bi-whatsapp" />
+        </div>
+        <div className="contact-text">
+          <p>+62 815-2720-5595 </p>
+        </div>
+      </div>
+    </div>
+    <div className="education-container">
+      <h2 className="title">Education</h2>
+      <div className="course">
+        <h2 className="education-title">Elementary School</h2>
+        <h5 className="college-name">GMIM PINOKALAN</h5>
+        <p className="education-date">2009 - 2015</p>
+      </div>
+      <div className="course">
+        <h2 className="education-title">Junior High School </h2>
+        <h5 className="college-name">NEGERI 1 BITUNG</h5>
+        <p className="education-date">2015 - 2018</p>
+      </div>
+      <div className="course">
+        <h2 className="education-title">Senior High School </h2>
+        <h5 className="college-name">NEGERI 1 BITUNG</h5>
+        <p className="education-date">2018 - 2021</p>
+      </div>
+    </div>
+    <div className="skills-container">
+      <h2 className="title">Skills</h2>
+      <div className="skill">
+        <div className="left-skill">
+          <p>HTML &ensp; &ensp;&ensp;90%</p>
+        </div>
+        <div className="right-skill">
+          <div className="outer-layer">
+            <div className="inner-layer" style={{width: '90%'}} />
+          </div>
+        </div>
+      </div>
+      <div className="skill">
+        <div className="left-skill">
+          <p>CSS &ensp;&ensp;&ensp;&ensp;&ensp; 70%</p>
+        </div>
+        <div className="right-skill">
+          <div className="outer-layer">
+            <div className="inner-layer" style={{width: '70%'}} />
+          </div>
+        </div>
+      </div>
+      <div className="skill">
+        <div className="left-skill">
+          <p>JS &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;85%</p>
+        </div>
+        <div className="right-skill">
+          <div className="outer-layer">
+            <div className="inner-layer" style={{width: '85%'}} />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="right-part">
+    <div className="banner">
+      <h1 className="firstname">Fransditho</h1>
+      <h1 className="lastname">Richardo</h1>
+      <p className="position"> web developer</p>
+    </div>
+    <div className="work-container ">
+      <h2 className="title text-left">ABOUT ME</h2>
+      <div className="work">
+        <div className="job-date">
+          <p className="job"></p>
+          <p className="date"></p>
+        </div>
+        <h2 className="company-name">Latar Belakang Pendidikan saya:</h2>
+        <p className="work-text">Saya adalah mahasiswa tahun kedua di Fakultas Ilmu Komputer, Universitas Klabat. Saat ini,
+         saya sedang mengejar gelar Sarjana Komputer dengan fokus pada Pengembangan Perangkat Lunak.</p>
+      </div>
+      <div className="work">
+        <div className="job-date">
+          <p className="job"></p>
+          <p className="date"></p>
+        </div>
+        <h2 className="company-name">Keahlian dan Keterampilan yang saya miliki:</h2>
+        <p className="work-text">Keahlian saya melibatkan bahasa pemrograman Python, Java, dan SQL. Selain itu, 
+        saya memiliki keterampilan mendesign Web didalam VS Code.</p>
+      </div>
+      <div className="work">
+        <div className="job-date">
+          <p className="job"></p>
+          <p className="date"></p>
+        </div>
+        <h2 className="company-name">Tujuan dan Aspirasi saya:</h2>
+        <p className="work-text">Dengan latar belakang ini, tujuan saya adalah menggabungkan pengetahuan teknis saya dengan kontribusi positif dalam pengembangan perangkat lunak yang inovatif. 
+        Saya berharap dapat memulai karir di industri teknologi setelah lulus.</p>
+      </div>
+      <div className="work">
+        <div className="job-date">
+          <p className="job"></p>
+          <p className="date"></p>
+        </div>
+        <h2 className="company-name">Jurusan yang saya tekuni saat ini:</h2>
+        <p className="work-text">Jurusan Teknik Informatika adalah program studi di perguruan tinggi yang memfokuskan pada teknologi informasi dan komputer. 
+        Siswa mempelajari pemrograman, perangkat lunak, perangkat keras, jaringan, sistem operasi, basis data, keamanan informasi, kecerdasan buatan, dan aplikasi teknologi seperti pengolahan citra dan suara. Tujuan program ini adalah untuk mempersiapkan siswa untuk karir di bidang pengembangan perangkat lunak, analisis data, keamanan informasi, dan bidang terkait lainnya.
+         Program ini sering melibatkan proyek akhir untuk menerapkan pengetahuan dalam situasi dunia nyata.</p>
+      </div>
+    </div>
+    <div className="references-container">
+      <h2 className="title text-left">achievments</h2>
+      <div className="references">
+        <div className="left-references">
+          <h4 className="name"></h4>          
+          <img src="img/HR.png" alt width="190" height="140"/>
+          <div className="phone">
+            <div className="phone-text">
+              <p></p>
+              <p></p>
+            </div>
+            <div className="phone-number">
+              <p></p>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <div className="right-references"> 
+          <h4 className="name">HackerRank</h4>
+          <p className="company-name">30 SEPTEMBER 2023</p>
+          <div className="phone">
+            <div className="phone-text"> CV
+              <p></p>
+              <p></p>
+            </div>
+            <div className="phone-number">
+              <p></p>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+        </>
+    )
+}
+export default Homepage
